9a2106b0240484301bba7bdc6d2a4d6c81e00050
etulyon1 <victor.andrade@etu.univ-lyon1.fr>
2025-11-13 10:10:55 +0100

    modif Table et Room

--- code/src/main/java/org/NeoMalokVector/SAE_S3/Room.java
+++ code/src/main/java/org/NeoMalokVector/SAE_S3/Room.java
@@ -15,19 +15,36 @@ public class Room
     {
     }
 
+    /**
+     * Fonction qui change la place de l'étudiant à la table num1 avec l'étudiant à la table num2.
+     * S'il n'y a pas d'étudiant à l'une des tables, change juste la place d'un étudiant
+     * @param num1 numéro de la première table
+     * @param num2 numéro de la deuxième table
+     */
     private void swapPlaces(int num1,  int num2)
     {
         Table temp = positioningIntermediate.tables[num1];
-        positioningIntermediate.tables[num1] = positioningIntermediate.tables[num2];
-        positioningIntermediate.tables[num2] = temp;
+        positioningIntermediate.tables[num1].student = positioningIntermediate.tables[num2].student;
+        positioningIntermediate.tables[num2].student = temp.student;
     }
 
+    /**
+     * Retire la table num des tables auxquelles un étudiant peut être placé.
+     * @param num numéro de la table retirée
+     */
     private void deleteTable(int num)
     {
-        positioningIntermediate.tables[num].destruction();
+        if (num==positioningIntermediate.tables[num].getId()){
+            positioningIntermediate.tables[num].destruction(true);
+        }
+
         positioningIntermediate.tables[num] = null;
     }
 
+    /**
+     * Exporte le placement sous les différents formats demandés.
+     * !!! Il faudra rajouter un moyen de choisir les différents formats d'export (3 booléens ou un string) !!!
+     */
     private void export()
     {
 
--- code/src/main/java/org/NeoMalokVector/SAE_S3/Table.java
+++ code/src/main/java/org/NeoMalokVector/SAE_S3/Table.java
@@ -2,13 +2,31 @@ package org.NeoMalokVector.SAE_S3;
 
 public class Table
 {
-    private static int qty=0;
+    private static int id=0;
     public int num;
     public Student student;
+
+    /**
+     * Constructeur par défaut de table, crée une table à la suite de la dernière table
+     */
     public Table(){
-        qty++;
-        num=qty;
+        id++;
+        num=id;
     }
+
+    /**
+     * Constructeur de la table à utilisée si on ne veut pas rajouter la table à la fin.
+     * Exemple: je veut remettre une table supprimée dans les tables.
+     * @param num numéro de la table
+     */
+    public Table(int num){
+        this.num=num;
+    }
+
+    /**
+     * Fonction qui permettra d'afficher les informations des tables dans le plan
+     * @return les informations de la table, soit son id si pas d'étudiant, soit son id+studentid si étudiant
+     */
     public String description()
     {
         String res=String.valueOf(num);
@@ -18,7 +36,30 @@ public class Table
             return res;
         }
     }
-    public void destruction(){
-        qty--;
+
+    /**
+     * getter du numéro de table
+     * @return num
+     */
+    public int getNum() {
+        return num;
+    }
+
+    /**
+     * getter du dernier id des tables
+     * @return id
+     */
+    public int getId(){
+        return id;
+    }
+
+    /**
+     *
+     * @param fin
+     */
+    public void destruction(boolean fin){
+        if (fin){
+            id--;
+        }
     }
 }

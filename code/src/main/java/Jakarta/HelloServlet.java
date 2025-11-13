e9113f3cfceb0250c1d549ee09b0e107db756373
Ramine0 <github.walmart106@passinbox.com>
2025-11-13 11:56:55 +0100

    Ajout de la page main

--- code/src/main/java/Jakarta/HelloServlet.java
+++ code/src/main/java/Jakarta/HelloServlet.java
@@ -4,7 +4,6 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.NeoMalokVector.SAE_S3.Room;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -12,22 +11,10 @@ import java.io.PrintWriter;
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet
 {
-    private String message;
-
-    public void init()
-    {
-        message = "Yoooooooooo comment ça va bien les gaaaaaaaaaaaaaars ?";
-    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
     {
         response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
     }
 
     public void destroy()

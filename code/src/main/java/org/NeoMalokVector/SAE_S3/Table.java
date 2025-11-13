55131aa9b186d7c2e20edf750a265ce94b052219
etulyon1 <victor.andrade@etu.univ-lyon1.fr>
2025-11-13 09:42:53 +0100

    Class Table et modif sur Room

--- code/src/main/java/org/NeoMalokVector/SAE_S3/Table.java
+++ code/src/main/java/org/NeoMalokVector/SAE_S3/Table.java
@@ -2,11 +2,23 @@ package org.NeoMalokVector.SAE_S3;
 
 public class Table
 {
+    private static int qty=0;
     public int num;
     public Student student;
-
-    public void description()
+    public Table(){
+        qty++;
+        num=qty;
+    }
+    public String description()
     {
-
+        String res=String.valueOf(num);
+        if (student!=null){
+            return (res+" "+student.id);
+        }else{
+            return res;
+        }
+    }
+    public void destruction(){
+        qty--;
     }
 }

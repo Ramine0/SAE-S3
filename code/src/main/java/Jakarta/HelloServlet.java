afdb822ca03e0e6655ce7d221f9c5f8bddc5b0d6
etulyon1 <victor.andrade@etu.univ-lyon1.fr>
2026-01-28 14:30:45 +0100

    Amélioration import

--- code/src/main/java/Jakarta/HelloServlet.java
+++ code/src/main/java/Jakarta/HelloServlet.java
@@ -14,19 +14,9 @@ public class HelloServlet
     {
         Room salle = new Room();
         CreatingIntermediate crea = salle.getCrea();
-        crea.createTables(3, 3);
-        crea.setDimensions(3, 3);
-        PositioningIntermediate pos;
-        pos = salle.getPositioningIntermediate();
-
-        crea.setMode(0);
-
-        crea.findStudentForGroup("p2406", 1);
-        crea.findStudentForGroup("p24033", 1);
-        crea.findStudentForGroup("p24039", 1);
-        salle.positioningMode() ;
-        System.out.println(salle.generate());
-        System.out.println(salle.getPositioningIntermediate().getTablesForVisu());
+        for (String s: crea.descripData()){
+            System.out.println(s);
+        }
 
     }
 

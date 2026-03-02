627fe0aaa333a3d9d59d7b86943455b72f2eeaea
etulyon1 <victor.andrade@etu.univ-lyon1.fr>
2026-03-02 16:26:44 +0100

    Commentaires creation

--- code/src/main/java/placement/CreatingIntermediate.java
+++ code/src/main/java/placement/CreatingIntermediate.java
@@ -8,39 +8,73 @@ import utilitaire.Utilitaire;
 
 import java.io.FileNotFoundException;
 
-
+/**
+ * Classe utilisée comme controlleur de la création des données (contraintes, étudiants...)
+ */
 public class CreatingIntermediate
 {
     private final Data d;
 
+    /**
+     * Constructeur par défaut de la classe.
+     * @throws FileNotFoundException envoyée par Data() si elle ne trouve pas de fichier
+     */
     public CreatingIntermediate() throws FileNotFoundException
     {
         d = new Data();
     }
 
+    /**
+     * Constructer de la classe. A quoi il sert, bonne question???
+     * @param path le chemin vers d'autres données?
+     * @throws FileNotFoundException envoyée par Data() si elle ne trouve pas de fichier
+     */
     public CreatingIntermediate(String path) throws FileNotFoundException
     {
         d = new Data(path, "D");
     }
 
+    /**
+     * Contrôle de création d'un plan rectangulaire
+     * @param lon nombre de tables en longueurs
+     * @param lar nombre de tables en largeurs
+     * @return true si la création est un succès, false sinon.
+     */
     public boolean createTables(int lon, int lar)
     {
         return d.setNumberTables(lon, lar);
     }
 
+    /**
+     * Fonction de récupération du nombre de tables
+     * @return le nombre de tables de la salle
+     */
     public int getNumberTables()
     {
         return d.getTables().length;
     }
 
+    /**
+     * Fonction de récupération du numéro de table minimum
+     * @return le numéro de table le plus faible
+     */
     public int minTable(){
         return d.minNumTable();
     }
 
+    /**
+     * Fonction de récupération du numéro de table maximum
+     * @return le numéro de table le plus important
+     */
     public int maxTable(){
         return d.maxNumTable();
     }
 
+    /**
+     * Fonction de recherche d'un étudiant à partir de l'id
+     * @param id numéro étudiant, complet ou non
+     * @return l'id complété ou un message adapté
+     */
     public String findEtu(String id)
     {
         String trouve = d.completeId(id);
@@ -57,12 +91,24 @@ public class CreatingIntermediate
 
     }
 
+    /**
+     * Vérifie que la table numTab soit dans les tables libres
+     * @param numTab le numéro de la table
+     * @return true si elle fait partie des tables libres, false sinon
+     */
     public boolean findTable(int numTab)
     {
         return Utilitaire.in(numTab, d.freeTables());
     }
 
 
+    /**
+     * Regarde si les informations id et num sont correctements renseignées, si c'est le cas appelle
+     * la logique d'ajout de places imposées de Data
+     * @param id numéro de l'étudiant
+     * @param num numéro de la table
+     * @return un entier correspondant à la situation
+     */
     public int findNumsForImp(String id, int num)
     {
         id = findEtu(id);
@@ -82,14 +128,32 @@ public class CreatingIntermediate
             return d.addImp(id, num);
         }
     }
+
+    /**
+     * Retire la contrainte de type constr et d'index id
+     * @param constr type de la contrainte
+     * @param id index de la contrainte
+     */
     public void removeContrainst(String constr, int id)
     {
         d.removeConstraint(constr, id);
     }
+
+    /**
+     * Renvoie le tableau de contraintes
+     * @return le tableau de contraintes
+     */
     public Constraint[] getConstr(){
         return d.getConstr();
     }
 
+    /**
+     * Vérifie que les informations soient correctemen renseignées, si c'est le cas entre dans
+     * la logique d'ajout d'étudiant au groupe numGrp
+     * @param idPartiel numéro de l'étudiant, complet ou non
+     * @param numGrp groupe auquel on veut ajouter l'étudiant
+     * @return en cas de succès le message de la logique dans data, sinon l'idPartiel
+     */
     public String findStudentForGroup(String idPartiel, int numGrp)
     {
         String etu = findEtu(idPartiel);
@@ -103,11 +167,20 @@ public class CreatingIntermediate
         }
     }
 
+    /**
+     * fonction utilisée pour afficher la description des données
+     * @return la description des données
+     */
     public String[] descripData()
     {
         return d.descrip();
     }
 
+    /**
+     * Fonction de récupération de l'étudiant de numéro étudiant num
+     * @param num numéro de l'étudiant
+     * @return null si l'étudiant n'est pas trouvé, son nom et son prénom sinon
+     */
     public String studentInfo(String num)
     {
         Student student = d.getStudentFromId(num);
@@ -119,6 +192,12 @@ public class CreatingIntermediate
         return student.getName() + " " + student.getFirstName();
     }
 
+    /**
+     * Cherche la table de numéro num, vérifie si elle est supprimée ou imposée, sinon entre dans
+     * la logique de suppression de la table
+     * @param num le numéro de la table
+     * @return un entier adapté à la situation (géré côté servlet et js)
+     */
     public int supprTable(int num)
     {
         num = findTable(num) ? num : -1;
@@ -139,36 +218,80 @@ public class CreatingIntermediate
         }
     }
 
+    /**
+     * tables libres
+     * @return les tables libres (non supprimées et sans étudiants/contrainte de place imposée
+     */
     public int[] free(){return d.freeTables();}
+
+    /**
+     * tables supprimées
+     * @return les tables supprimées
+     */
     public int[] del(){return d.getDeletedTables();}
+
+    /**
+     * tables existantes
+     * @return les tables existantes (non supprimées)
+     */
     public int[] existing(){return d.existingTables();}
 
+    /**
+     * entre dans la logique de réinsertion de table supprimées pour la table num
+     * @param num numéro de la table
+     */
     public void unremoveTable(int num)
     {
         d.unremoveTable(num);
     }
 
+    /**
+     * définition des dimensions du plan
+     * @param lon longueur du plan
+     * @param lar largeur du plan
+     */
     public void setDimensions(int lon, int lar) {d.setDimensions(lon, lar);}
+
+    /**
+     * fonction de récupération de la map
+     * @return la map
+     */
     public Map getMap()
     {
         return d.getMap();
     }
 
+    /**
+     * fonction de récupération des étudiants imposés
+     * @return un tableau contenant les numéros étuidants de tous les étudiants imposés
+     */
     public String[] getImposedStud()
     {
         return d.imposedStudents();
     }
 
+    /**
+     * fonction de réinitialisation des données
+     */
     public void resetData()
     {
         d.reset();
     }
 
+    /**
+     * fonction de récupération de l'étudiant assis à la table num
+     * @param num le numéro de la table
+     * @return l'étudiant assis à la table num
+     */
     public Student StuFromTable(int num)
     {
         return d.getStuFromTab(num);
     }
 
+    /**
+     * fonction d'obtention du nombre de contraintes
+     * @return la longueur
+     */
     public int getNbConstr()
     {
         return d.getConstr().length;
@@ -238,6 +361,10 @@ public class CreatingIntermediate
         return d.tableExist(numTab) ;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getSeparated() {
         String result = "";
         for (int i = 1; i < 10 ; i++ ) {
@@ -259,6 +386,10 @@ public class CreatingIntermediate
         return result ;
     }
 
+    /**
+     * affichage des étudiants utilisé dans une fenêtre popup
+     * @return un string avec la liste d'étudiants
+     */
     public String getStudentList() {
         String result = "ID             ; nom prenom \n";
         result += d.studentList() ;
